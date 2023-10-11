b937259e87b46edb03cf1a3db391cf801b0f7b34
Juliusquell <julius-quell@web.de>
2023-10-11 10:34:43 +0200

    implementation of livestream

--- gettingstarted.py
+++ gettingstarted.py
@@ -1,2 +1,25 @@
 import cv2
 
+cap = cv2.VideoCapture(2)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while cap.isOpened():
+
+    ret, img = cap.read()
+
+    h, w, _ = img.shape
+
+    width = 1000
+    height = int(width * (h / w))
+    img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+
+    cv2.imshow("Image", img)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
